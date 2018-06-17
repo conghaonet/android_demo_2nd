1b66328ce0ade1714516139496a411ce4bed5359
conghao <hao.cong@app2m.com>
2018-06-17 19:54:50 +0800

    配置databinding, 添加MainVM

--- app/src/main/java/com/app2m/demo2/MainActivity.kt
+++ app/src/main/java/com/app2m/demo2/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        mBinding = DataBindingUtil.setContentView<Demo2ActivityMainBinding>(this, R.layout.demo2_activity_main)
         mBinding.activity = this
+        mBinding.model = MainVM()
     }
 
     fun onClickPermissions(view: View) {
